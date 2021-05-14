288bdbcb9fd88fb8a2961a6738339a3ac16201b1
jundymek <jundymek@gmail.com>
2021-05-14 12:16:09 +0200

    style: Job box fully styled

--- src/components/jobs/Jobs.vue
+++ src/components/jobs/Jobs.vue
@@ -1,10 +1,19 @@
 <template>
   <div class="wrapper">
-    <div v-for="job in filteredJobs" :key="job.id" class="box">
+    <div
+      v-for="job in filteredJobs"
+      :key="job.id"
+      class="box"
+      :class="{ isFeatured: job.featured }"
+    >
       <div class="left">
         <img :src="require(`@/images/${job.logo}`)" alt="" class="logo" />
         <div class="content">
-          <h2 class="title">{{ job.company }}</h2>
+          <div class="title-wrapper">
+            <h2 class="title">{{ job.company }}</h2>
+            <Badge type="new" v-if="job.new" />
+            <Badge type="featured" v-if="job.featured" />
+          </div>
           <span class="position">{{ job.position }}</span>
           <div class="options">
             <span>{{ job.postedAt }}</span>
@@ -42,8 +51,10 @@
 <script lang="ts">
 import { defineComponent } from "vue";
 import { useFilterJobs } from "@/composables/useFilterJobs";
+import Badge from "./Badge.vue";
 
 export default defineComponent({
+  components: { Badge },
   setup() {
     const { filteredJobs, addSortElement, sortBy } = useFilterJobs();
     console.log(filteredJobs.value);
@@ -59,10 +70,27 @@ export default defineComponent({
 }
 .box {
   background: #fff;
+  box-sizing: border-box;
   width: 80%;
-  margin: 10px auto;
-  padding: 10px 0;
+  height: 154px;
+  margin: 20px auto;
+  padding: 10px 20px;
   display: flex;
+  border-radius: 4px;
+  position: relative;
+  box-shadow: 0px 6px 35px -18px hsl(180, 29%, 50%);
+
+  &.isFeatured::before {
+    position: absolute;
+    content: "";
+    left: 0;
+    top: 0;
+    border-top-left-radius: 4px;
+    border-bottom-left-radius: 4px;
+    height: 100%;
+    width: 5px;
+    background-color: $desaturatedDarkCyan;
+  }
 }
 .left,
 .right {
@@ -77,8 +105,8 @@ export default defineComponent({
 }
 
 .logo {
-  width: 112px;
-  height: 112px;
+  width: 86px;
+  height: 86px;
 }
 
 .content {
@@ -86,24 +114,38 @@ export default defineComponent({
   flex-direction: column;
   align-items: flex-start;
   width: 100%;
-  margin-left: 40px;
+  margin-left: 20px;
+}
+
+.title-wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 14px;
 }
 
 .title {
   font-size: 14px;
+  margin: 0;
   color: $desaturatedDarkCyan;
 }
 
 .position {
-  font-size: 16px;
+  font-size: 18px;
   font-weight: 600;
+  cursor: pointer;
+  transition: 0.1s;
+  margin-bottom: 4px;
+  &:hover {
+    color: $desaturatedDarkCyan;
+  }
 }
 
 .options {
   display: flex;
   width: 100%;
   margin: 10px 0 0 -10px;
-  font-size: 12px;
+  font-size: 14px;
   color: $darkGrayishCyan;
 
   span {
@@ -122,8 +164,18 @@ export default defineComponent({
 
 .badge {
   margin: 0 10px;
-  padding: 5px 10px;
+  padding: 6px 10px;
   background: $lightGrayishCyan;
+  border: 0;
+  cursor: pointer;
   border-radius: 4px;
+  font-size: 12px;
+  font-weight: 600;
+  color: $desaturatedDarkCyan;
+  transition: 0.1s;
+  &:hover {
+    background: $desaturatedDarkCyan;
+    color: #fff;
+  }
 }
 </style>
