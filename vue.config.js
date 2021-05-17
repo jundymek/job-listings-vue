feba7befb2a74be9fc3ac85c23978f7f403db07e
jundymek <jundymek@gmail.com>
2021-05-17 09:49:55 +0200

    chore: gh-pages config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === "production" ? "/ip-tracker-vue/" : "/",
+  publicPath:
+    process.env.NODE_ENV === "production" ? "/job-listings-vue/" : "/",
   css: {
     loaderOptions: {
       sass: {
